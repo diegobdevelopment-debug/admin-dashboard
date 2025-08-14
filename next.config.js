93c1e4254849f38d270e9090e2d064af3ffd6c8c
Diego <diegob.development@gmail.com>
2025-08-14 09:43:12 -0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  crossOrigin: 'anonymous',
+  reactStrictMode: false,
+  env: {
+    // Replace below URL with your current Domain 
+    API_PROD_URL: "http://localhost:3000/api/",
+    storageURL: "http://localhost:3000/assets", 
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,  
+      },
+      {
+        source: "/en",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1:3000",
+      },
+    ],
+  }
+};
+
+module.exports = nextConfig;
